7b52f37c1bde9ae8c15c48f7c69959ea24a39eac
Javier Bauset <baunes@protonmail.com>
2020-04-26 15:26:35 +0200

    Commit inicial

---
+++ src/main/java/com/prueba/controlfichajes/ControlFichajesApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.controlfichajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlFichajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlFichajesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/controlfichajes/ControlFichajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.controlfichajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlFichajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
